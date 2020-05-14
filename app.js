0ee75c37ec4c7f209e0d8870dde682eb1ab00aa0
Maksym <maksymRozhko7@gmail.com>
2020-05-14 03:33:37 +0300

    created array
    User sets length and sets elements.
    Then sorting and deleted array elements 2 to 4 (inclusive!).
    As the array changes, the output of content to the page.

---
+++ app.js
@@ -0,0 +1,25 @@
+//1.Создать массив, длину и элементы которого задаёт пользователь.
+// Затем отсортировать массив по возрастанию.
+// Затем, удалить элементы из массива с 2 по 4 (включительно!).
+// По мере изменений, выводить содержимое массива на страницу.
+
+let digitElement, userDigit, arr = [];
+
+function compareDigit(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+}
+
+userDigit = parseInt(prompt('Set array length'));
+
+for (let i = 0; i < userDigit; i++) {
+    digitElement = parseInt(prompt('Enter digit #' + (i + 1)));
+    arr.push(digitElement);
+}
+document.write('<p/>' + arr + '<p/>');
+arr.sort(compareDigit);
+document.write('<p/>' + arr + '<p/>');
+let arrPart = arr.splice(1,3);
+document.write('<p/>' + arr + '<p/>');
+document.write('<p/>' + arrPart + '<p/>');
