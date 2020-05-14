cd49290672fb799f319906adea4b271a4d377a49
Maksym <maksymRozhko7@gmail.com>
2020-05-14 03:37:47 +0300

    added new js file, created array and variable

---
+++ app2.js
@@ -0,0 +1,12 @@
+const arr = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+let positiveNumbersSum = 0, positiveNumbersElements = [], // a.
+    minElement = 0, minElementIndexNumber, 
+    maxElement = 0, maxElementIndexNumber, 
+    negativeNumbersElements = [], 
+    positiveNumbersElementsOdd = [], 
+    positiveNumbersElementsEven = [], 
+    sumEvenPositive = 0,
+    sumOddPositive = 0,
+    productOfPositiveNumbers = 1,
+    nullifyNumbers; 
+    
